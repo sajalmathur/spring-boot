62c4de6042430fe9769e0716aa0f8df74b2c828b
sajal.mathurs@gmail.com <sajal.mathurs@gmail.com>
2019-09-03 00:56:44 +0530

    spring-boot-setup

---
+++ src/main/java/com/sample/project/springboot/Application.java
@@ -0,0 +1,27 @@
+package com.sample.project.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(Application.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping(value="/")
+	public String hello() {
+		return "Hello from Spring-Boot application";
+	}
+}
